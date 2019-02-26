0b1d065af66b8efccdd5a511920ccef8ff6d96ae
edenlee143@gmail.com <edenlee143@gmail.com>
2019-02-26 20:33:03 +0800

    recaptcha validation 2.5

--- js/style.js
+++ js/style.js
@@ -37,6 +37,13 @@ $(document).ready(function() {
       if (response.length === 0) {
         console.log("captcha mandatory");
       }
+      if (fullname == "" || email == "" || message == "") {
+        //   $(".form-feedback-err").html("All fields are required")
+        $(".contact-form-err").empty();
+        $(".contact-form-err").fadeIn(1000);
+        $(".contact-form-err").append("There was an error while submitting the form. Please try again!");
+        $(".contact-form-err").fadeOut(3000);
+      }
       else {
             httpAjaxFD("post", "contactsent", fd).then(res => {
               $(".contact-form-success").fadeIn(1000);
